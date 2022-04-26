4ce82869dfce802c3833261b38f627c2eb779a03
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-26 10:16:40 +0600

    Create cat.cpp

---
+++ Searching_Algorithm/Binary_search/Coins And Triangle(best)/cat.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+ll check(ll mid,ll n)
+{
+    return ((mid*(mid+1))/2<=n);
+}
+void solve()
+{
+    ll n,ans=-1,mid=0;
+    cin>>n;
+    ll l=1,r =1e7;
+    while(l<=r)
+    {
+       mid = l+(r-l)/2;
+       if(check(mid,n))
+       {
+           ans = max(ans,mid);
+           l =mid+1;
+       }
+       else
+       {
+           r =mid-1;
+       }
+    }
+    cout<<ans<<endl;
+}
