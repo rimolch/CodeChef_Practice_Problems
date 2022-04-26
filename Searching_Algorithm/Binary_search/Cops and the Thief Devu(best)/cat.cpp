5b7f8868382d9ac78b48455b838b06364fe2bc27
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-26 12:49:06 +0600

    Update cat.cpp

--- Searching_Algorithm/Binary_search/Cops and the Thief Devu(best)/cat.cpp
+++ Searching_Algorithm/Binary_search/Cops and the Thief Devu(best)/cat.cpp
@@ -1,3 +1,13 @@
+/*Here, is the pseudo code for this question. 
+take a look.
+ans = 0
+for each house from 1 to 100:
+        safe = true
+for each cop houses from 1 to N:
+if (the cop can reach the house)
+safe = false
+if (safe) ans += 1*/
+
 #include<bits/stdc++.h>
 
 using namespace std;
