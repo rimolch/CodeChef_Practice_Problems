f22c1a462a492da76be31d8fe801087517548a06
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-26 10:18:31 +0600

    Create cat.cpp

---
+++ Searching_Algorithm/Binary_search/Cops and the Thief Devu(best)/cat.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	int t;
+	cin>>t;
+   while(t--)
+   {
+	int m,x,y;
+	cin>>m>>x>>y;
+	//int dist = x*y;
+	vector<int>ar(m);
+	for(int i=0;i<m;i++)
+    {
+        cin>>ar[i];
+    }
+    int dist = x*y;
+    int count =0;
+    //bool is_safe = true;
+    for(int i=1;i<=100;i++)
+    {
+         bool is_safe = true;
+        for(int j=0;j<m;j++)
+        {
+            int l = max(ar[j]-dist,1);
+            int r = min(100,ar[j]+dist);
+            if(i>=l&&i<=r)
+            {
+                is_safe = false;
+            }
+        }
+        if(is_safe)
+        {
+            count+=1;
+        }
+    }
+    cout<<count<<endl;
+   }
+    return 0;
+}
