78d3415d0607cff0e2dba8923a913d223227d373
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-27 09:14:56 +0600

    Update tw.cpp

--- Searching_Algorithm/Binary_search/The Wave (less)/tw.cpp
+++ Searching_Algorithm/Binary_search/The Wave (less)/tw.cpp
@@ -43,4 +43,49 @@ int main()
      }
     checkmate;
 }
+//another method
+/*#include<bits/stdc++.h>
 
+using namespace std;
+
+#define checkmate return 0
+
+#define ll long long int
+
+int main()
+{
+
+    int n,q;
+    cin>>n>>q;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];
+    }
+    sort(ar,ar+n);
+    while(q--)
+    {
+        int x;
+        cin>>x;
+       int *ptr= lower_bound(ar,ar+n,x);
+      if(*ptr==x)
+      {
+         cout<<"0"<<endl;
+      }
+      else
+      {
+        int x=ptr-ar+1;
+        if(x&1)
+        {
+           cout<<"POSITIVE"<<endl;
+        }
+        else
+        {
+           cout<<"NEGATIVE"<<endl;
+        }
+      }
+    }
+    checkmate;
+}
+
+*/
