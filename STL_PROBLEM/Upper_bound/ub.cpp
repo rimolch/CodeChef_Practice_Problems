270ff8789383dd0a555b0fa198998a79919bfa97
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-29 17:31:53 +0600

    Update ub.cpp

--- STL_PROBLEM/Upper_bound/ub.cpp
+++ STL_PROBLEM/Upper_bound/ub.cpp
@@ -27,3 +27,34 @@ int main()
     checkmate;
 }
 
+//using vector
+/*#include<bits/stdc++.h>
+
+using namespace std;
+
+#define checkmate return 0
+
+unordered_map<char,int>symbols ={{'(',-1},{'{',-2},{'[',-3},{')',1},{'}',2},{']',3}};
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    vector<int>ar(n);
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];//1 2 6 4 4
+    }
+    sort(ar.begin(),ar.end());
+     for(int i=0;i<n;i++)
+    {
+        cout<<ar[i]<<" ";//1 2 4 4 6
+    }
+    cout<<endl;
+    auto it =upper_bound(ar.begin(),ar.end(),4);//6
+    cout<<*it<<endl;
+    checkmate;
+}
+
+*/
