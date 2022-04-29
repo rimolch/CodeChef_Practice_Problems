352137d297daa3087b7fd9fe2408361a3f8c4a86
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-29 08:23:59 +0600

    Create another_one_balance_parenthesis.cpp

---
+++ STL_PROBLEM/Balanced_paranthesis/another_one_balance_parenthesis.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+unordered_map<char,int>symbols ={{'(',-1},{'[',-2},{'{',-3},{')',1},{']',2},{'}',3}};
+
+string is_balanced(string s)
+{
+    stack<char>st;
+    for(char char_bracket_in_string:s)
+    {
+         if(symbols[char_bracket_in_string]<0)
+         {
+             st.push(char_bracket_in_string);
+         }
+         else
+         {
+             if(st.empty())
+             {
+                 return "NO";
+             }
+             else
+             {
+                 char top =st.top();
+                 st.pop();
+                 if(symbols[top]+symbols[char_bracket_in_string]!=0)
+                 {
+                    return "NO";
+                 }
+             }
+         }
+    }
+    if(st.empty())
+    {
+        return "YES";
+    }
+    else
+    {
+       return "NO";
+    }
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        cout<<is_balanced(s)<<endl;
+    }
+    return 0;
+}
