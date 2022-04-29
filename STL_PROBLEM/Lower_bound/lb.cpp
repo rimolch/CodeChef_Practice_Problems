c9754778f241ec91feafbb3dd8e19a0ed4b6a830
rimolch <67545874+rimolch@users.noreply.github.com>
2022-04-29 17:26:12 +0600

    Create lb.cpp

---
+++ STL_PROBLEM/Lower_bound/lb.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define checkmate return 0
+
+unordered_map<char,int>symbols ={{'(',-1},{'{',-2},{'[',-3},{')',1},{'}',2},{']',3}};
+
+int main()
+{
+
+    int n;
+    cin>>n;
+    int ar[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>ar[i];//1 2 6 4 4
+    }
+    sort(ar,ar+n);
+     for(int i=0;i<n;i++)
+    {
+        cout<<ar[i]<<" ";//1 2 4 4 6
+    }
+    cout<<endl;
+    int *ptr =lower_bound(ar,ar+n,5);//6
+    cout<<*ptr<<endl;
+    checkmate;
+}
+
