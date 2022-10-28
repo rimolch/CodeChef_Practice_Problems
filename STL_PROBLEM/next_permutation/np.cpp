f2b6fbf4486614035d7922ed1f816afe5accbb07
rimolch <67545874+rimolch@users.noreply.github.com>
2022-10-28 10:30:37 +0600

    Update np.cpp

--- STL_PROBLEM/next_permutation/np.cpp
+++ STL_PROBLEM/next_permutation/np.cpp
@@ -3,14 +3,18 @@ using namespace std;
 
 int main()
 {
-    int n=5;
+    int n;
+    cin>>n;
     vector<int>v(n);
     for(int i=0;i<n;i++){
         cin>>v[i];
     }
     sort(v.begin(),v.end());
     while(next_permutation(v.begin(),v.end())){
-        cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<" "<<v[3]<<" "<<v[4]<<endl;
+        for(int i=0;i<n;i++){
+            cout<<v[i]<<" ";
+        }
+        cout<<endl;
     }
     return 0;
 }
